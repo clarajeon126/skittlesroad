d502d88f1bedbd1fe7814e99d740b405bc5b4b46
clarajeon <clarajeon126@gmail.com>
2022-03-27 12:05:15 -0400

    first commit

---
+++ Assets/Scripts/FirewallButtonText.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class FirewallButtonText : MonoBehaviour
+{
+
+    public Main main;
+    public TextMeshProUGUI tex1;
+    public TextMeshProUGUI tex2;
+    public butt yes;
+    public butt no;
+
+
+    void Start() {
+        tex1.text = "Play\ncost: $" + (50 * Mathf.Pow(3, main.level));
+        tex2.text = "Decline\ncost: $" + (100 * Mathf.Pow(3, main.level));
+    }
+
+    private void FixedUpdate()
+    {
+
+        tex1.text = "Play\ncost: $" + (5 * Mathf.Pow(3, main.level));
+        tex2.text = "Decline\ncost: $" + (10 * Mathf.Pow(3, main.level));
+
+
+
+
+        if (main.money >= (10 * Mathf.Pow(3, main.level))){
+            yes.enable();
+            no.enable();
+        }
+        else if(main.money >= (5 * Mathf.Pow(3, main.level))){
+            no.disable();
+            yes.enable();
+        }
+        else
+        {
+            yes.disable();
+            no.disable();
+
+        }
+
+
+    }
+
+
+    public void loserRoute() {
+
+        main.money -= 200 * Mathf.Pow(4, main.level);
+        main.riskMulti *= 0.8f;
+    }
+}
